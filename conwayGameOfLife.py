869ae961a3321188c1ebfff7ec61286b17b1b15b
jemro774 <31485871+jemro774@users.noreply.github.com>
2017-12-14 14:45:19 -0500

    Updated from Brython Server: 12/14/2017 2:45:15 PM

--- conwayGameOfLife.py
+++ conwayGameOfLife.py
@@ -1,1 +1,13 @@
+#Jack Robey 
+#12/14/17 
+#conwayGameOfLife.py 
 
+buildBoard(): 
+    for row in range(0,10): 
+        for col in range(0,10): 
+            print(board[row][col],' ',end = '') 
+        print() 
+
+buildBoard() 
+
+if __name__ == '__main__':
