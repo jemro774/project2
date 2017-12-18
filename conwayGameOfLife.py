92222627a7ebe8480df8c6f4582191c02b275d65
jemro774 <31485871+jemro774@users.noreply.github.com>
2017-12-18 14:25:45 -0500

    Updated from Brython Server: 12/18/2017 2:25:44 PM

--- conwayGameOfLife.py
+++ conwayGameOfLife.py
@@ -2,12 +2,25 @@
 #12/14/17 
 #conwayGameOfLife.py 
 
+from ggame import *
+
+black = Color(0x000000,1)
+white = Color(0xFFFFFF,1)
+
+blackOutline = LineStyle(1,black)
+
+square = RectangleAsset(75,75,blackOutline,white)
+
 buildBoard(): 
     for row in range(0,10): 
         for col in range(0,10): 
             print(board[row][col],' ',end = '') 
-        print() 
-
-buildBoard() 
+        print()
 
 if __name__ == '__main__':
+
+buildBoard()
+
+Sprite(square)
+App().run()
+
