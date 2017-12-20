0444572a3b04e26acae62044d21cafd7eb6a472a
jemro774 <31485871+jemro774@users.noreply.github.com>
2017-12-20 14:00:46 -0500

    Updated from Brython Server: 12/20/2017 2:00:46 PM

--- conwayGameOfLife.py
+++ conwayGameOfLife.py
@@ -9,7 +9,8 @@ white = Color(0xFFFFFF,1)
 
 blackOutline = LineStyle(1,black)
 
-square = RectangleAsset(40,40,blackOutline,white)
+whiteSquare = RectangleAsset(40,40,blackOutline,white)
+blackSquare = RectangleAsset(40,40,blackOutline,black)
 
 def buildBoard():
     board = [0*10,0*10,0*10,0*10,0*10,0*10,0*10,0*10,0*10,0*10]
@@ -17,11 +18,30 @@ def buildBoard():
 def redrawAll(): 
     for row in range(0,10): 
         for col in range(0,10): 
-            Sprite(square,(40*row+100,40*col+64))
+            Sprite(whiteSquare,(40*row,40*col))
+
+def mouseClick(event):
+    if event.x > 0 and event.x < 40 and event.y > 0 and event.y < 40:
+        Sprite(blackSquare,(0,0))
+    elif event.x > 0 and event.x < 40 and event.y > 40 and event.y < 80:
+        Sprite(blackSquare,(0,40))
+    elif event.x > 0 and event.x < 40 and event.y > 80 and event.y < 120:
+        Sprite(blackSquare,(0,80))
+    elif event.x > 0 and event.x < 40 and event.y > 120 and event.y < 160:
+        Sprite(blackSquare,(0,120))
+    elif event.x > 0 and event.x < 40 and event.y > 160 and event.y < 200:
+        Sprite(blackSquare,(0,160))
+    elif event.x > 0 and event.x < 40 and event.y > 200 and event.y < 240:
+        Sprite(blackSquare,(0,200))
+    elif event.x > 0 and event.x < 40 and event.y > 240 and event.y < 280:
+        Sprite(blackSquare,(0,240))
 
 if __name__ == '__main__':
 
+    buildBoard()
+    
     redrawAll()
     
+    App().listenMouseEvent('click',mouseClick)
     App().run()
 
