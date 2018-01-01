be3dd4a742bee265871b1921e93001722a2953d7
jemro774 <31485871+jemro774@users.noreply.github.com>
2018-01-01 13:42:39 -0500

    Updated from Brython Server: 1/1/2018 1:42:38 PM

--- conwayGameOfLife.py
+++ conwayGameOfLife.py
@@ -21,22 +21,10 @@ def redrawAll():
             Sprite(whiteSquare,(40*row,40*col))
 
 def mouseClick(event):
-    if event.x > 0 and event.x < 40 and event.y > 0 and event.y < 40:
-        Sprite(blackSquare,(0,0))
-    elif event.x > 0 and event.x < 40 and event.y > 40 and event.y < 80:
-        Sprite(blackSquare,(0,40))
-    elif event.x > 0 and event.x < 40 and event.y > 80 and event.y < 120:
-        Sprite(blackSquare,(0,80))
-    elif event.x > 0 and event.x < 40 and event.y > 120 and event.y < 160:
-        Sprite(blackSquare,(0,120))
-    elif event.x > 0 and event.x < 40 and event.y > 160 and event.y < 200:
-        Sprite(blackSquare,(0,160))
-    elif event.x > 0 and event.x < 40 and event.y > 200 and event.y < 240:
-        Sprite(blackSquare,(0,200))
-    elif event.x > 0 and event.x < 40 and event.y > 240 and event.y < 280:
-        Sprite(blackSquare,(0,240))
-    elif event.x > 0 and event.x < 40 and event.y > 280 and event.y < 320:
-        Sprite(blackSquare,(0,280))
+    for row in range(0,10): 
+        for col in range(0,10):
+            if event.x > 40*row and event.x < 40*row+40 and event.y > 40*col and event.y < 40*col+40:
+                Sprite(blackSquare,(40*row,40*col))
 
 if __name__ == '__main__':
 
